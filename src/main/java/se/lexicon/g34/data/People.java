f3f1b6aed16448319ab0a5da1fb59c53b52be2ce
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2020-12-31 16:02:37 +0100

    Class People is added and tested.

---
+++ src/main/java/se/lexicon/g34/data/People.java
@@ -0,0 +1,54 @@
+package se.lexicon.g34.data;
+
+
+import se.lexicon.g34.model.Person;
+import se.lexicon.g34.data.PersonSequencer;
+
+import java.util.Arrays;
+
+public class People {
+    private static Person[] personList = new Person[0];
+    private static PersonSequencer counter=new PersonSequencer();
+    //Constructor
+    public People() {
+    }
+
+    //methods
+    public int size() {
+        return personList.length;
+    }
+
+    public Person[] findAll() {
+        return personList;
+
+    }
+
+    public Person findByID(int findPerson) {
+        if (findPerson>=0 ) {
+            for (Person checkPerson : personList) {
+                if (findPerson == checkPerson.getPersonId()) {
+                    return checkPerson;
+                }
+            }
+        }
+        return new Person(-1,"Invalid","PersonID");//With person set to -1 the caller can take care of not existing
+
+    }
+
+    public Person addPerson(String firstName,String lastName) {
+        int newId=counter.nextPersonId();
+        Person newPerson=new Person(newId,firstName,lastName);
+        personList=Arrays.copyOf(personList,size()+1);
+        personList[size()-1]=newPerson;
+        return newPerson;
+    }
+
+    public void clear() {
+        clearPersonList();
+    }
+
+
+    private void clearPersonList() {
+        personList = new Person[0];
+    }
+}
---
+++ src/test/java/se/lexicon/g34/data/PeopleTest.java
@@ -0,0 +1,51 @@
+package se.lexicon.g34.data;
+
+import org.junit.Before;
+import org.junit.Test;
+import se.lexicon.g34.model.Person;
+
+
+public class PeopleTest {
+
+
+    @Test
+    public void testPeople() { //test size(), addPerson() and findAll()
+        People peopleList = new People();
+        peopleList.addPerson("Björn", "Larsson");
+        peopleList.addPerson("Sven", "Ohlsson");
+        peopleList.addPerson("Ida", "Ohlsson");
+        peopleList.addPerson("Harriet", "Larsson");
+        peopleList.addPerson("Gösta", "Larsson");
+        printPersonList(peopleList.findAll());
+        System.out.println(peopleList.size() + " persons in the list");
+        System.out.println("------------------------------------------"); //Testing findById by searching for all numbers if many
+        Person searchPerson;
+        for (int i = -1; i < peopleList.size() + 1; i++) {//If Persons is removed and new is added this test will not find them.
+            System.out.println("Search for person with Id=" + i);
+            searchPerson = peopleList.findByID(i);
+            if (searchPerson.getPersonId() == -1) {
+                System.out.println("Person with Id " + i + " can't be found");
+                continue;
+            }
+            printPerson(searchPerson);
+        }
+        System.out.println("------------------------------------------");//Testing clear()
+        System.out.println("Clear the list");
+        peopleList.clear();
+        printPersonList(peopleList.findAll());
+        System.out.println(peopleList.size() + " persons in the list");
+    }
+
+    public void printPersonList(Person[] list) {
+        for (Person listPerson : list) {
+            printPerson(listPerson);
+        }
+    }
+
+    public void printPerson(Person personToPrint) {
+        System.out.print(personToPrint.getPersonId() + ". ");
+        personToPrint.printFullName();
+        System.out.print("\n");
+    }
+
+}
